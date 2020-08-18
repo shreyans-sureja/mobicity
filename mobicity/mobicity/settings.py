ee155c4c1e090131139937c107184254393f5453
Shreyans <28798785+shreyans-sureja@users.noreply.github.com>
2020-08-18 23:48:11 +0530

    Update settings.py

--- mobicity/mobicity/settings.py
+++ mobicity/mobicity/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lb12*9sgcp#ypz%hg*+)r@po-x9nkprdwbe^_#=p3tmcf0ta-('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
